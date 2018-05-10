72d069f7629fc0a67059c90fb0c6b724d234b5e1
JL <JuanLuis.Gonzalez@adidas-group.com>
2018-05-10 20:11:32 +0200

    Refactoring

--- src/main/java/com/adidas/controller/InventoryController.java
+++ src/main/java/com/adidas/controller/InventoryController.java
@@ -19,78 +19,65 @@ import org.springframework.core.env.Environment;
 
 import java.util.List;
 
-
 @RestController
 public class InventoryController {
 
-	//Histogram
-	static final Histogram requestLatency = Histogram.build()
-			.name("http_request_duration_seconds")
-			.help("HTTP request duration (seconds).")
-			.labelNames("method")
-			.register();    // Register must be called to add it to the output
+	// Histogram
+	static final Histogram requestLatency = Histogram.build().name("http_request_duration_seconds")
+			.help("HTTP request duration (seconds).").labelNames("method").register(); // Register must be called to add
+																						// it to the output
 
-	//Counter
-	static final Counter requestsCounter = Counter.build()
-			.name("requests_total")
-			.help("Total requests.")
-			.labelNames("method")
-			.register();
+	// Counter
+	static final Counter requestsCounter = Counter.build().name("requests_total").help("Total requests.")
+			.labelNames("method").register();
 
 	private static final Log log = LogFactory.getLog(InventoryController.class);
 
 	@Autowired
 	InventoryRepository inventoryRepository;
 
-
-	@Autowired
-	RestTemplate restTemplate;
-	@Bean
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	}
-	
 	@GetMapping("/inventory")
 	public Iterable<Inventory> getInventory() {
-		//Counter: added 1 when we access to the /path
+		// Counter: added 1 when we access to the /path
 		requestsCounter.labels("getInventory").inc();
- 		//Histogram: time request
+		log.info("getInventory - ");
+		// Histogram: time request
 		Histogram.Timer requestTimer = requestLatency.labels("getInventory").startTimer();
 		try {
 			return inventoryRepository.findAll();
 		} finally {
-			//Histogram:  Stop the histogram timer.
+			// Histogram: Stop the histogram timer.
 			requestTimer.observeDuration();
 		}
 	}
-	
+
 	@GetMapping("/inventory/{modelId}")
-	public List<Inventory> getModelInventory(@PathVariable String modelId){
-		//Counter: added 1 when we access to the /path
+	public List<Inventory> getModelInventory(@PathVariable String modelId) {
+		// Counter: added 1 when we access to the /path
 		requestsCounter.labels("getModelInventory").inc();
- 		//Histogram: time request
+		// Histogram: time request
 		Histogram.Timer requestTimer = requestLatency.labels("getModelInventory").startTimer();
 		try {
 			return inventoryRepository.findByModelId(modelId);
 		} finally {
-			//Histogram:  Stop the histogram timer.
+			// Histogram: Stop the histogram timer.
 			requestTimer.observeDuration();
 		}
 	}
 
 	@GetMapping("/inventory/{modelId}/warehouse/{warehouseId}")
-	public  List<Inventory> getModelInventoryByWarehouse( @PathVariable String modelId, @PathVariable String warehouseId){
-		//Counter: added 1 when we access to the /path
+	public List<Inventory> getModelInventoryByWarehouse(@PathVariable String modelId,
+			@PathVariable String warehouseId) {
+		// Counter: added 1 when we access to the /path
 		requestsCounter.labels("getModelInventoryByWarehouse").inc();
- 		//Histogram: time request
+		// Histogram: time request
 		Histogram.Timer requestTimer = requestLatency.labels("getModelInventoryByWarehouse").startTimer();
 		try {
-			return inventoryRepository.findByModelIdAndWarehouseId(modelId,warehouseId);
+			return inventoryRepository.findByModelIdAndWarehouseId(modelId, warehouseId);
 		} finally {
-			//Histogram: Stop the histogram timer.
+			// Histogram: Stop the histogram timer.
 			requestTimer.observeDuration();
 		}
 	}
-
 
 }
